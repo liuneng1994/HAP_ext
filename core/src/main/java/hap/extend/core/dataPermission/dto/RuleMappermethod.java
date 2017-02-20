b0c491295d8e8bd9bb3335f83501cfe601e54b41
Yangyazheng <2017948437@qq.com>
2017-02-20 16:11:16 +0800

    mapper

--- core/src/main/java/hap/extend/core/dataPermission/dto/RuleMappermethod.java
+++ core/src/main/java/hap/extend/core/dataPermission/dto/RuleMappermethod.java
@@ -3,10 +3,7 @@ package hap.extend.core.dataPermission.dto;
 import com.hand.hap.mybatis.annotation.Condition;
 import com.hand.hap.system.dto.BaseDTO;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * HAP扩展功能之数据屏蔽：规则与mapper方法的映射关系
@@ -25,8 +22,13 @@ public class RuleMappermethod extends BaseDTO {
     @Condition
     private Long ruleId;
     @Column(name = "mapper_method")
-    @Condition(operator = LIKE)
-    private String mapperMethod;
+    @Condition
+    private Long mapperMethod;
+
+    @Transient
+    private String ruleName;
+    @Transient
+    private String mapperMethodName;
 
     public Long getMapperId() {
         return mapperId;
@@ -44,11 +46,11 @@ public class RuleMappermethod extends BaseDTO {
         this.ruleId = ruleId;
     }
 
-    public String getMapperMethod() {
+    public Long getMapperMethod() {
         return mapperMethod;
     }
 
-    public void setMapperMethod(String mapperMethod) {
+    public void setMapperMethod(Long mapperMethod) {
         this.mapperMethod = mapperMethod;
     }
 }
---
+++ core/src/main/java/hap/extend/core/dataPermission/mapper/RuleMappermethodMapper.java
@@ -0,0 +1,12 @@
+package hap.extend.core.dataPermission.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hap.extend.core.dataPermission.dto.RuleMappermethod;
+
+/**
+ * Created by yyz on 2017/2/20.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+public interface RuleMappermethodMapper extends Mapper<RuleMappermethod> {
+}
