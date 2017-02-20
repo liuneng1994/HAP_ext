e8f98c6ff3b2987380b2ff9aa74cc8c7f879eb82
Yangyazheng <2017948437@qq.com>
2017-02-20 15:56:53 +0800

    常量以及缓存工具

--- core/src/main/java/hap/extend/core/dataPermission/utils/CacheUtils.java
+++ core/src/main/java/hap/extend/core/dataPermission/utils/CacheUtils.java
@@ -7,21 +7,38 @@ package hap.extend.core.dataPermission.utils;
  */
 public final class CacheUtils {
     /**
-     * 根据数据库中的规则id生成redis缓存中的规则id
+     * 根据数据库中的规则id,
+     * 生成redis缓存中的规则key
      * @param ruleId
      * @return
      */
-    public static String getRuleId(final String ruleId){
-        return Constant.FIELD_METHOD_ID_PREFIX+ruleId;
+    public static String getRuleKey(final String ruleId){
+        return Constant.FIELD_RULE_ID_PREFIX+ruleId;
     }
 
     /**
-     * 根据mapper方法的id和用户id生成redis缓存中的数据权限规则的id
+     * 根据mapper方法的id,
+     * 生成redis缓存中的key,
+     * 用于快速找到mapper方法需要应用的规则id数组
      * @param idOfMapperMethod
-     * @param userId
      * @return
      */
-    public static String getRuleIdsOfUserMethodMap(final String idOfMapperMethod, final String userId){
-        return Constant.FIELD_METHOD_USER_MAP_PREFIX+idOfMapperMethod+"_"+userId;
+    public static String getMappermethodRulesKey(final String idOfMapperMethod){
+        return Constant.FIELD_METHOD_RULE_MAP_PREFIX+idOfMapperMethod;
+    }
+
+    /**
+     * 生成redis缓存中的key,
+     * 用于开始判断当前用户是否在这个规则上生效
+     * @param ruleId
+     * @param userId
+     * @param isInclude
+     * @return
+     */
+    public static String getRuleUserKey(final String ruleId, final String userId, boolean isInclude){
+        if(isInclude){
+            return Constant.FIELD_RULE_USER_PREFIX+ruleId+"_"+userId+"_Y";
+        }
+        return Constant.FIELD_RULE_USER_PREFIX+ruleId+"_"+userId+"_N";
     }
 }
--- core/src/main/java/hap/extend/core/dataPermission/utils/Constant.java
+++ core/src/main/java/hap/extend/core/dataPermission/utils/Constant.java
@@ -8,12 +8,20 @@ package hap.extend.core.dataPermission.utils;
  * @author yazheng.yang@hand-china.com
  */
 public final class Constant {
-    /** redis缓存中键值对表hap_extend.data_permission的key前缀 */
-    public static final String FIELD_METHOD_USER_MAP_PREFIX="EXT_DATA_PERM_";
-    /** redis缓存中键值对表hap_extend.rules的key前缀 */
-    public static final String FIELD_METHOD_ID_PREFIX="EXT_DATA_PERM_RULE_";
-    /** redis键值对表名称,，(key,value)=(key,规则id数组) */
-    public static final String FIELD_METHOD_USER_MAP_CACHE_NAME="hap_extend.data_permission";
+    /** redis mapper方法对应于哪些规则的键值对表名称，(key,value)=(key,规则id数组) */
+    public static final String FIELD_METHOD_RULES_MAP_CACHE_NAME ="hap_extend.mappermethod_rules";
     /** redis权限规则表名称，(key,value)=(rule_id,规则sql片段) */
     public static final String FIELD_RULES_CACHE_NAME="hap_extend.rules";
+    /** redis 规则是否需要应用到用户的映射表名称，(key,value)=(包含rule_id、user_id、是否应用标志位,字符串"Y") */
+    public static final String FIELD_RULE_USER_CACHE_NAME="hap_extend.rule_user";
+
+    /** redis缓存中键值对表hap_extend.data_permission的key前缀 */
+    public static final String FIELD_METHOD_RULE_MAP_PREFIX="RULES_";
+    /** redis缓存中键值对表hap_extend.rules的key前缀 */
+    public static final String FIELD_RULE_ID_PREFIX="RULE_";
+    /** redis缓存中键值对表hap_extend.rule_user的key前缀 */
+    public static final String FIELD_RULE_USER_PREFIX="RULE_USER_";
+
+    public static final String VALUE_RULE_USER = "Y";
+
 }
