b48ce1d88473cbe429d8a08aa0fd5769ac762b17
Yangyazheng <2017948437@qq.com>
2017-02-20 15:56:37 +0800

    lan常用工具

---
+++ core/src/main/java/hap/extend/core/dataPermission/utils/LangUtils.java
@@ -0,0 +1,18 @@
+package hap.extend.core.dataPermission.utils;
+
+/**
+ * Created by yyz on 2017/2/20.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+public class LangUtils {
+
+    public static boolean isNull(Object object){
+        return null == object;
+    }
+
+    public static boolean isNotNull(Object object){
+        return null != object;
+    }
+
+}
