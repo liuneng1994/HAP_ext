5d3fcec0ae9e8ffbf3bcb29ea493ecd99edf3cc6
Yangyazheng <2017948437@qq.com>
2017-03-08 13:36:26 +0800

    fix bug

--- core/src/main/java/hap/extend/core/operation/component/mapper/ComponentTypeMapper.java
+++ core/src/main/java/hap/extend/core/operation/component/mapper/ComponentTypeMapper.java
@@ -5,5 +5,6 @@ import com.hand.hap.mybatis.common.Mapper;
 /**
  * Created by liuneng on 2017/2/28.
  */
-public interface ComponentTypeMapper extends Mapper {
+//public interface ComponentTypeMapper extends Mapper {
+public interface ComponentTypeMapper{
 }
