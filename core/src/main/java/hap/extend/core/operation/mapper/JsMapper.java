142e8014a99db3293f14e52b000c6866aa058ed5
Yangyazheng <2017948437@qq.com>
2017-03-13 17:07:44 +0800

    js code mapper

---
+++ core/src/main/java/hap/extend/core/operation/mapper/JsMapper.java
@@ -0,0 +1,12 @@
+package hap.extend.core.operation.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hap.extend.core.operation.dto.Js;
+
+/**
+ * Created by yyz on 2017/3/10.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+public interface JsMapper extends Mapper<Js> {
+}
