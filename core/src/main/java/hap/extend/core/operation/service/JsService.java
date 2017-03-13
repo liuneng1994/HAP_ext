2593461da30063efeb01c80e7f975265fb024966
Yangyazheng <2017948437@qq.com>
2017-03-13 17:08:04 +0800

    js service

---
+++ core/src/main/java/hap/extend/core/operation/service/JsService.java
@@ -0,0 +1,13 @@
+package hap.extend.core.operation.service;
+
+import com.hand.hap.core.ProxySelf;
+import com.hand.hap.system.service.IBaseService;
+import hap.extend.core.operation.dto.Js;
+
+/**
+ * Created by yyz on 2017/3/13.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+public interface JsService extends IBaseService<Js>,ProxySelf<JsService> {
+}
---
+++ core/src/main/java/hap/extend/core/operation/service/impl/JsServiceImpl.java
@@ -0,0 +1,15 @@
+package hap.extend.core.operation.service.impl;
+
+import com.hand.hap.system.service.impl.BaseServiceImpl;
+import hap.extend.core.operation.dto.Js;
+import hap.extend.core.operation.service.JsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yyz on 2017/3/13.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+@Service
+public class JsServiceImpl extends BaseServiceImpl<Js> implements JsService {
+}
