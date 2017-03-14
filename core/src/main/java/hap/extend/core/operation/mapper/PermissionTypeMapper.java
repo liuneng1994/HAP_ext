e6d9e9951d39854171cf71c121e922145fde35f6
Yangyazheng <2017948437@qq.com>
2017-03-14 15:16:24 +0800

    permission type mapper

---
+++ core/src/main/java/hap/extend/core/operation/mapper/PermissionTypeMapper.java
@@ -0,0 +1,13 @@
+package hap.extend.core.operation.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hap.extend.core.operation.dto.PermissionType;
+
+/**
+ * Created by yyz on 2017/3/13.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+public interface PermissionTypeMapper extends Mapper<PermissionType> {
+
+}
