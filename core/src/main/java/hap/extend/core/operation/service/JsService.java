7ee1c4a3301864c04edb32e8f70fbac473f435bc
Yangyazheng <2017948437@qq.com>
2017-03-16 14:26:41 +0800

    js service annotated for short term

--- core/src/main/java/hap/extend/core/operation/service/JsService.java
+++ core/src/main/java/hap/extend/core/operation/service/JsService.java
@@ -1,8 +1,11 @@
 package hap.extend.core.operation.service;
 
+import com.hand.hap.core.IRequest;
 import com.hand.hap.core.ProxySelf;
 import com.hand.hap.system.service.IBaseService;
 import hap.extend.core.operation.dto.Js;
+
+import java.util.List;
 
 /**
  * Created by yyz on 2017/3/13.
@@ -10,4 +13,6 @@ import hap.extend.core.operation.dto.Js;
  * @author yazheng.yang@hand-china.com
  */
 public interface JsService extends IBaseService<Js>,ProxySelf<JsService> {
+//    public List<Js> selectPageJsByRole(IRequest request,Long resourceId,Long roleId);
+//    public List<Js> selectPageJsByUser(IRequest request,Long resourceId,Long userId);
 }
