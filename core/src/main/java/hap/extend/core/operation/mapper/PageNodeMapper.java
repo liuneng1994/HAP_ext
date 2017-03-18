0f690a06494fe0476156f50ab63b50f3dc5dcf37
Yangyazheng <2017948437@qq.com>
2017-03-18 21:45:02 +0800

    pageNode and its mapper

---
+++ core/src/main/java/hap/extend/core/operation/mapper/PageNodeMapper.java
@@ -0,0 +1,14 @@
+package hap.extend.core.operation.mapper;
+
+import hap.extend.core.operation.dto.PageNode;
+
+import java.util.List;
+
+/**
+ * Created by yyz on 2017/3/17.
+ *
+ * @author yazheng.yang@hand-china.com
+ */
+public interface PageNodeMapper {
+    public List<PageNode> selectAllPageNode();
+}
