7add90f6b7c2ced6ec9a212c302f2c4157efcf03
Yangyazheng <2017948437@qq.com>
2017-03-23 09:10:30 +0800

    delete useless code:html file scanner

--- core/src/main/java/hap/extend/core/operation/scanner/converter/TemplateConverter.java
+++
@@ -1,8 +0,0 @@
-package hap.extend.core.operation.scanner.converter;
-
-/**
- * 非标准HTML代码作为HTML代码
- */
-public interface TemplateConverter {
-    String convertToHtml(String template);
-}
--- core/src/main/java/hap/extend/core/operation/scanner/marker/Marker.java
+++
@@ -1,9 +0,0 @@
-package hap.extend.core.operation.scanner.marker;
-
-/**
- * 模板标记。用于判断是否包含特殊标签，以及特殊标签替换成标准HTML代码片段
- */
-public interface Marker {
-    boolean isMarker(String marker);
-    String getReplacement(String marker);
-}
