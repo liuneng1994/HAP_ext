fcf0fb8c0bb842b6e882ec2c265d118d94330bca
Yangyazheng <2017948437@qq.com>
2017-03-24 09:20:32 +0800

    template and train of thought of apply fixed rule

--- core/src/main/java/hap/extend/core/operation/utils/OPConstUtil.java
+++ core/src/main/java/hap/extend/core/operation/utils/OPConstUtil.java
@@ -12,6 +12,11 @@ public final class OPConstUtil {
     public static final String VALUE_USER_TYPE = "USER";
     public static final String VALUE_GLOBAL_TYPE = "GLOBAL";
 
+    public static final String VALUE_GRID_LEVEL = "GRID";
+    public static final String VALUE_FORM_LEVEL = "FORM";
+    public static final String JS_FUNCTION_NAME_FORMAT = "%s_%s_%sFunction('%s','%s')";
+    public static final String JS_COMMON_FUNCTION_NAME_FORMAT = "COMMON_%s_%sFunction('%s','%s')";
+
 
     public static boolean isEnable(String enableFlagStr){
         return VALUE_YES.equals(enableFlagStr);
@@ -19,4 +24,29 @@ public final class OPConstUtil {
     public static boolean isDisable(String enableFlagStr){
         return VALUE_NO.equals(enableFlagStr);
     }
+
+    /**
+     * generate the js function name of special format
+     * @param level
+     * @param cpnType
+     * @param opType -- visible/require/readonly/disable
+     * @param htmlTagId
+     * @return js function name
+     */
+    public static String generateJsFunName(String level, String cpnType, String opType, String htmlTagId, String value){
+        return String.format(JS_FUNCTION_NAME_FORMAT, level, cpnType, opType, htmlTagId, value);
+    }
+
+    /**
+     * generate the common js function name of special format
+     * @param level
+     * @param opType -- visible/require/readonly/disable
+     * @param htmlTagId
+     * @return js function name
+     */
+    public static String generateJsCommonFunName(String level, String opType, String htmlTagId, String value){
+        return String.format(JS_COMMON_FUNCTION_NAME_FORMAT, level, opType, htmlTagId, value);
+    }
+
+    
 }
