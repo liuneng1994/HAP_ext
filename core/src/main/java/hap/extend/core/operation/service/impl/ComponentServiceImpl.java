b4ee6a822450bbc99bb4bf440ae562f1e8e9dd6e
Yangyazheng <2017948437@qq.com>
2017-03-27 10:53:05 +0800

    component service

---
+++ core/src/main/java/hap/extend/core/operation/service/impl/ComponentServiceImpl.java
@@ -0,0 +1,13 @@
+package hap.extend.core.operation.service.impl;
+
+import com.hand.hap.system.service.impl.BaseServiceImpl;
+import hap.extend.core.operation.dto.Component;
+import hap.extend.core.operation.service.ComponentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yyz on 17/3/24.
+ */
+@Service
+public class ComponentServiceImpl extends BaseServiceImpl<Component> implements ComponentService {
+}
