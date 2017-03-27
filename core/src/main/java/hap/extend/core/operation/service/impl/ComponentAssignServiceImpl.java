dcb4c8efcc4ec20a7a677c0b3e9b0126feafd7d5
Yangyazheng <2017948437@qq.com>
2017-03-27 10:54:09 +0800

    Component Assign Service

---
+++ core/src/main/java/hap/extend/core/operation/service/impl/ComponentAssignServiceImpl.java
@@ -0,0 +1,13 @@
+package hap.extend.core.operation.service.impl;
+
+import com.hand.hap.system.service.impl.BaseServiceImpl;
+import hap.extend.core.operation.dto.ComponentAssign;
+import hap.extend.core.operation.service.ComponentAssignService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yyz on 17/3/24.
+ */
+@Service
+public class ComponentAssignServiceImpl extends BaseServiceImpl<ComponentAssign> implements ComponentAssignService {
+}
