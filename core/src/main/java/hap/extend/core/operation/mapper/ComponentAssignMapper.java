373591c052fae3d48e87ce4b04d320d4a1ede927
Yangyazheng <2017948437@qq.com>
2017-03-27 10:55:06 +0800

    Component Assign Mapper

---
+++ core/src/main/java/hap/extend/core/operation/mapper/ComponentAssignMapper.java
@@ -0,0 +1,10 @@
+package hap.extend.core.operation.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hap.extend.core.operation.dto.ComponentAssign;
+
+/**
+ * Created by yyz on 17/3/24.
+ */
+public interface ComponentAssignMapper extends Mapper<ComponentAssign> {
+}
