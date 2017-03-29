b2e41f44b22b6452e566d8d1215785fcdcd1b60c
Yangyazheng <2017948437@qq.com>
2017-03-29 15:14:55 +0800

    fetch db type config

--- core/src/main/java/hap/extend/core/dataPermission/utils/ConfigUtil.java
+++ core/src/main/java/hap/extend/core/dataPermission/utils/ConfigUtil.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- * read config properties for data permission
+ * read config properties for data permission.
  * Created by yyz on 17/3/29.
  */
 public final class ConfigUtil {
     private static Properties properties;
     public static boolean enableWrapSqlWithSelectAll = false;
+    public static String dbType;
 
     static {
         properties = new Properties();
         try {
             properties.load(ConfigUtil.class.getClassLoader().getResourceAsStream("config.properties"));
             String flag = properties.getProperty("hap_extend.data_permission.enable_wrap_sql_with_select_all.flag","false");
+            dbType = properties.getProperty("db.type","");
             if("true".equalsIgnoreCase(flag)){
                 enableWrapSqlWithSelectAll = true;
             }
