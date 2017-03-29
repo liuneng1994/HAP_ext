aa9fda830260e9dec8669d69ca6d192a88b2d66c
Yangyazheng <2017948437@qq.com>
2017-03-29 09:25:23 +0800

    config util for data permission

---
+++ core/src/main/java/hap/extend/core/dataPermission/utils/ConfigUtil.java
@@ -0,0 +1,26 @@
+package hap.extend.core.dataPermission.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * read config properties for data permission
+ * Created by yyz on 17/3/29.
+ */
+public final class ConfigUtil {
+    private static Properties properties;
+    public static boolean enableWrapSqlWithSelectAll = false;
+
+    static {
+        properties = new Properties();
+        try {
+            properties.load(ConfigUtil.class.getClassLoader().getResourceAsStream("config.properties"));
+            String flag = properties.getProperty("hap_extend.data_permission.enable_wrap_sql_with_select_all.flag","false");
+            if("true".equalsIgnoreCase(flag)){
+                enableWrapSqlWithSelectAll = true;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
