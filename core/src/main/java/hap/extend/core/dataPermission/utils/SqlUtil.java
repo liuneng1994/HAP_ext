c1f363b87f1a2806302a97cbb6f9cdc9938ce434
Yangyazheng <2017948437@qq.com>
2017-03-30 11:11:16 +0800

    SQL util for wrap select all in top level

--- core/src/main/java/hap/extend/core/dataPermission/utils/SqlUtil.java
+++ core/src/main/java/hap/extend/core/dataPermission/utils/SqlUtil.java
@@ -3,12 +3,14 @@ package hap.extend.core.dataPermission.utils;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.statement.select.PlainSelect;
-import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static hap.extend.core.dataPermission.utils.LangUtils.isNotNull;
 import static hap.extend.core.dataPermission.utils.LangUtils.isNull;
 
 /**
@@ -20,6 +22,8 @@ public final class SqlUtil {
     public static Logger logger = LoggerFactory.getLogger(SqlUtil.class);
 
     public static final String FIELD_SQL_AND = "AND";
+    public static Pattern limitOffsetPattern = Pattern.compile("LIMIT \\? OFFSET \\?");
+    public static Pattern limitPattern = Pattern.compile("LIMIT \\?,\\?");
 
     /**
      * generate new sql with additional Condition
@@ -32,7 +36,63 @@ public final class SqlUtil {
         if(isNull(conditionSql)){
             return oldSql;
         }
-        Select select = (Select) CCJSqlParserUtil.parse(oldSql);
+        String newSql = oldSql;
+
+        if(ConfigUtil.enableWrapSqlWithSelectAll && "MYSQL".equalsIgnoreCase(ConfigUtil.dbType)){
+            String source = oldSql.toUpperCase();
+            boolean flag = false;
+            String tempSql = oldSql;
+
+            if(source.endsWith("LIMIT ? OFFSET ?")){
+                int lastIndex = source.lastIndexOf("LIMIT ? OFFSET ?");
+                tempSql = oldSql.substring(0,lastIndex);
+                flag = true;
+            }
+            if(source.endsWith("LIMIT ?,?")){
+                int lastIndex = source.lastIndexOf("LIMIT ?,?");
+                tempSql = oldSql.substring(0,lastIndex);
+                flag = true;
+            }
+
+            if(flag){
+                newSql = "SELECT HAP_DP_TEMP.* FROM ("+ tempSql+") HAP_DP_TEMP LIMIT ?,?";
+            }else {
+                newSql = "SELECT HAP_DP_TEMP.* FROM ("+ tempSql+") HAP_DP_TEMP";
+            }
+        }
+        else if(!ConfigUtil.enableWrapSqlWithSelectAll && "ORACLE".equalsIgnoreCase(ConfigUtil.dbType)){
+            Select oldSelect = (Select) CCJSqlParserUtil.parse(newSql);
+            String oldSqlStrGenerated = oldSelect.toString();
+            Expression pageWhere = ((PlainSelect) oldSelect.getSelectBody()).getWhere();
+            if(isNotNull(pageWhere) && "row_id > ?".equalsIgnoreCase(pageWhere.toString())){
+                FromItem fromItem = ((PlainSelect) oldSelect.getSelectBody()).getFromItem();
+                if(isNotNull(fromItem)){
+                    String temp_1 = fromItem.toString().substring(1,fromItem.toString().length()-1);
+                    Select nestedSelect = (Select) CCJSqlParserUtil.parse(temp_1);
+                    if(isNotNull(nestedSelect)){
+                        FromItem sourceSqlFromIem = ((PlainSelect) nestedSelect.getSelectBody()).getFromItem();
+                        int lastIndex = sourceSqlFromIem.toString().lastIndexOf(")");
+                        String sourceSql = sourceSqlFromIem.toString().substring(1,lastIndex);
+                        Select sourceSelect = (Select) CCJSqlParserUtil.parse(sourceSql);
+                        Expression where = ((PlainSelect) sourceSelect.getSelectBody()).getWhere();
+                        Expression expression = null;
+                        if(isNull(where) || where.toString().length() < 1){
+                            expression = CCJSqlParserUtil.parseCondExpression(conditionSql);
+                        }else {
+                            expression = CCJSqlParserUtil.parseCondExpression(where.toString() + " " + FIELD_SQL_AND + " " + conditionSql);
+                        }
+                        ((PlainSelect) sourceSelect.getSelectBody()).setWhere(expression);
+
+                        oldSqlStrGenerated = oldSqlStrGenerated.replace(sourceSql,sourceSelect.toString());
+
+                        logger.info("\nsql with data permission:\n{}\n",oldSqlStrGenerated);
+                        return oldSqlStrGenerated;
+                    }
+                }
+            }
+        }
+
+        Select select = (Select) CCJSqlParserUtil.parse(newSql);
         Expression where = ((PlainSelect) select.getSelectBody()).getWhere();
         Expression expression = null;
         if(isNull(where) || where.toString().length() < 1){
@@ -41,7 +101,7 @@ public final class SqlUtil {
             expression = CCJSqlParserUtil.parseCondExpression(where.toString() + " " + FIELD_SQL_AND + " " + conditionSql);
         }
         ((PlainSelect) select.getSelectBody()).setWhere(expression);
-        logger.info("\n\nsql with data permission:\n{}\n\n",select.toString());
+        logger.info("\nsql with data permission:\n{}\n",select.toString());
         return select.toString();
     }
 
@@ -53,8 +113,8 @@ public final class SqlUtil {
         if(!source.contains("LIMIT")){
             return sql;
         }
-        Pattern p = Pattern.compile("LIMIT \\? OFFSET \\?");
-        Matcher m = p.matcher(source);
+
+        Matcher m = limitOffsetPattern.matcher(source);
         return m.replaceAll("LIMIT \\?,\\?");
     }
 }
