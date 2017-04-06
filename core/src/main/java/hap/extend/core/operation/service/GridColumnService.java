82b14b6a019e23280259aa1381bd28139a5cbb25
Yangyazheng <2017948437@qq.com>
2017-04-06 09:54:25 +0800

    service for GridColumn

---
+++ core/src/main/java/hap/extend/core/operation/service/GridColumnService.java
@@ -0,0 +1,11 @@
+package hap.extend.core.operation.service;
+
+import com.hand.hap.core.ProxySelf;
+import com.hand.hap.system.service.IBaseService;
+import hap.extend.core.operation.dto.GridColumn;
+
+/**
+ * Created by yyz on 17/4/1.
+ */
+public interface GridColumnService extends IBaseService<GridColumn>,ProxySelf<GridColumnService> {
+}
---
+++ core/src/main/java/hap/extend/core/operation/service/impl/GridColumnServiceImpl.java
@@ -0,0 +1,13 @@
+package hap.extend.core.operation.service.impl;
+
+import com.hand.hap.system.service.impl.BaseServiceImpl;
+import hap.extend.core.operation.dto.GridColumn;
+import hap.extend.core.operation.service.GridColumnService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yyz on 17/4/1.
+ */
+@Service
+public class GridColumnServiceImpl extends BaseServiceImpl<GridColumn> implements GridColumnService {
+}
