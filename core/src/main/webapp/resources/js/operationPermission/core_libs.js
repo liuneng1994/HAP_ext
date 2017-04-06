45d0ccaa89fd458fe066fe5a7102fd6935446000
Yangyazheng <2017948437@qq.com>
2017-04-06 09:49:21 +0800

    grid relative control js-version1

--- core/src/main/webapp/resources/js/operationPermission/core_libs.js
+++ core/src/main/webapp/resources/js/operationPermission/core_libs.js
@@ -153,3 +153,304 @@ function gridUtils_deleteButtonInColumn(param_tagAttributeValue) {
 }
 
 // =====================GRID OPERATION================================================end
+
+
+
+// =====================BASIC OPERATION================================================start
+function core_apply_action(param_tagAttr, param_tagAttrVal, param_val, param_callbackFunction) {
+    var temp = "["+param_tagAttr+"='"+param_tagAttrVal+"']";
+    $.each($(temp), function (key, value) {
+        param_callbackFunction($(value),param_val);
+    });
+}
+
+function core_extractElementId(param_ele) {
+    var id_str = param_ele.attr("ID");
+    return id_str+"";
+}
+
+function core_hasAppliedRule(param_ele) {
+    var mask_str = param_ele.attr("op_pms_mask");
+    if(mask_str && mask_str.toUpperCase()=="APPLIED"){
+        return true;
+    }
+    return false;
+}
+
+function core_setAppledMask(param_ele) {
+    param_ele.attr("op_pms_mask","APPLIED");
+}
+// =====================BASIC OPERATION================================================end
+
+
+
+
+
+
+// =====================GRID COMMON OPERATION================================================start
+function COMMON_GRID_displayFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if(!core_hasAppliedRule(ele)){
+            if("N"===val){
+                ele.attr("hidden","hidden");
+                ele.hide();
+                core_setAppledMask(ele);
+            }
+            else if("Y"===val){
+                ele.show();
+                core_setAppledMask(ele);
+            }
+        }
+    });
+}
+
+function COMMON_GRID_requireFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.attr("required","required");
+        }
+        else if("N"===val){
+            ele.removeAttr("required");
+        }
+    });
+}
+
+function COMMON_GRID_readonlyFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.attr("readonly","readonly");
+        }
+        else if("N"===val){
+            ele.removeAttr("readonly");
+        }
+    });
+}
+
+function COMMON_GRID_disableFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.attr("disabled","disabled");
+            ele.unbind();
+            ele.prop("onclick",null);
+            ele.click(function (event) {
+                event.preventDefault();
+            });
+        }
+        else if("N"===val){
+            //suggest do nothing
+            ele.removeAttr("disabled");
+        }
+    });
+}
+// =====================GRID COMMON OPERATION================================================end
+
+
+
+// =====================GRID grid OPERATION================================================start
+function GRID_grid_displayFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+
+        }
+        else if("N"===val){
+            ele.attr("hidden","hidden");
+            ele.hide();
+        }
+    });
+}
+function GRID_grid_requireFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        //just do nothing for whole grid
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+function GRID_grid_readonlyFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            gridUtils_forbidEditWholeGrid(core_extractElementId(ele));
+        }
+        else if("N"===val){
+            gridUtils_allowEditWholeGrid(core_extractElementId(ele));
+        }
+    });
+}
+function GRID_grid_disableFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.attr("disabled","disabled");
+        }
+        else if("N"===val){
+            //suggest do nothing
+            // ele.removeAttr("disabled");
+        }
+    });
+}
+// =====================GRID grid OPERATION================================================end
+
+
+// =====================GRID grid column OPERATION================================================start
+//TODO 重新设计参数模板
+/**
+ * just handle hide columns
+ * @param param_tagAttr
+ * @param param_tagAttrVal
+ * @param param_val index array of columns(is going to be hidden),start from 0;
+ * @constructor
+ */
+function GRID_grid_column_displayFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+
+        }
+        else if("N"===val){
+            var idStr = core_extractElementId(ele);
+            gridUtils_hideColumns(idStr,param_val);
+        }
+    });
+}
+/**
+ * <b>目前无合适解决方案</b>
+ * @param param_tagAttr
+ * @param param_tagAttrVal
+ * @param param_val
+ * @constructor
+ */
+function GRID_grid_column_requireFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+function GRID_grid_column_readonlyFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            gridUtils_forbidEditColumns(core_extractElementId(ele),param_val);
+        }
+        else if("N"===val){
+            //do nothing
+        }
+    });
+}
+/**
+ * <b>目前无合适解决方案</b>
+ * @param param_tagAttr
+ * @param param_tagAttrVal
+ * @param param_val
+ * @constructor
+ */
+function GRID_grid_column_disableFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+// =====================GRID grid column OPERATION================================================end
+
+
+
+// =====================GRID button OPERATION================================================start
+function GRID_button_displayFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.removeAttr("hidden","hidden");
+            ele.show();
+        }
+        else if("N"===val){
+            ele.attr("hidden","hidden");
+            ele.hide();
+        }
+    });
+}
+function GRID_button_requireFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        //just do nothing for button
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+function GRID_button_readonlyFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        //just do nothing for button
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+function GRID_button_disableFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.attr("disabled","disabled");
+            ele.unbind();
+            ele.prop("onclick",null);
+            ele.click(function (event) {
+                event.preventDefault();
+            });
+        }
+        else if("N"===val){
+            //suggest do nothing, for we have removed all events,...
+        }
+    });
+}
+// =====================GRID button OPERATION================================================end
+
+
+
+// =====================FORM button OPERATION================================================start
+function FORM_button_displayFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.removeAttr("hidden","hidden");
+            ele.show();
+        }
+        else if("N"===val){
+            ele.attr("hidden","hidden");
+            ele.hide();
+        }
+    });
+}
+function FORM_button_requireFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        //just do nothing for button
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+function FORM_button_readonlyFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        //just do nothing for button
+        if("Y"===val){
+        }
+        else if("N"===val){
+        }
+    });
+}
+function FORM_button_disableFunction(param_tagAttr, param_tagAttrVal, param_val) {
+    core_apply_action(param_tagAttr,param_tagAttrVal,param_val,function (ele, val) {
+        if("Y"===val){
+            ele.attr("disabled","disabled");
+            ele.unbind();
+            ele.prop("onclick",null);
+            ele.click(function (event) {
+                event.preventDefault();
+            });
+        }
+        else if("N"===val){
+            //do nothing
+        }
+    });
+}
+// =====================FORM button OPERATION================================================end
+
+
