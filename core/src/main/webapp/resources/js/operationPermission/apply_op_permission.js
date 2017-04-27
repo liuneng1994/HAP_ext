8433bcbfa8d12ba8788daccce238281e23b71143
Yangyazheng <2017948437@qq.com>
2017-04-27 15:36:43 +0800

    fix：special for endless creation of HTML tag

--- core/src/main/webapp/resources/js/operationPermission/apply_op_permission.js
+++ core/src/main/webapp/resources/js/operationPermission/apply_op_permission.js
@@ -3,21 +3,15 @@
  */
 
 $(document).ready(function(){
-    var HAP_EXTEND_OPERATION_APPLY_FUNCTION_POINTER = 0;
     var HAP_EXTEND_OPERATION_APPLY_FUNCTION_REFER;
 
     HAP_EXTEND_OPERATION_APPLY_FUNCTION_REFER = setInterval(function () {
         try{
             eval(op_permission_code);
         }catch (err){
-            HAP_EXTEND_OPERATION_APPLY_FUNCTION_POINTER=250;
             clearInterval(HAP_EXTEND_OPERATION_APPLY_FUNCTION_REFER);
             // console.error(err);
             throw err;
-        }
-        HAP_EXTEND_OPERATION_APPLY_FUNCTION_POINTER ++;
-        if(HAP_EXTEND_OPERATION_APPLY_FUNCTION_POINTER > 250){
-            clearInterval(HAP_EXTEND_OPERATION_APPLY_FUNCTION_REFER);
         }
     },200);
 });
